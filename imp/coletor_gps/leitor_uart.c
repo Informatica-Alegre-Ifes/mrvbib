8c98c97bdd7fd2821794a38956375b3eceee5c83
Rpmoura7 <rpmoura7@gmail.com>
2017-07-21 04:05:26 +0000

    30082017

--- imp/coletor_gps/leitor_uart.c
+++ imp/coletor_gps/leitor_uart.c
@@ -5,7 +5,51 @@
 #include <unistd.h>
 #include <string.h>
 
+int InicializarLeitura();
+void InicializarTermios(int uart0_file);
+unsigned char ObterDados(int uart0_file);
+void QuebrarLinha(unsigned char protocolo);
+
+struct horario
+{
+	short int hora;
+	short int minuto;
+	short int segundo;
+};
+
+struct data
+{
+	short int dia;
+	short int mes;
+	short int ano;
+};
+
+struct gps_data
+{
+	struct horario horario;
+	float latitude;
+	float longitude;
+	float velocidade;
+	float angulo;
+	struct data data;	
+};
+
 int main()
+{
+	unsigned char protocolo;
+	struct gps_data gps;
+	int leitor_uart0 = InicializarLeitura();
+	InicializarTermios(leitor_uart0);
+	while(1)
+	{
+	 	protocolo = ObterDados(leitor_uart0);
+		QuebrarLinha(protocolo);
+	}
+		 
+	return (0);
+}
+
+int InicializarLeitura()
 {
 	int uart0_file = -1;
 	
@@ -16,6 +60,11 @@ int main()
 		printf("ERRO!");
 		exit(1);
 	}
+	return uart0_file;
+}
+
+void InicializarTermios(int uart0_file)
+{
 	
 	struct termios options;
 	tcgetattr(uart0_file, &options);
@@ -26,7 +75,58 @@ int main()
 
 	tcflush(uart0_file, TCIFLUSH);
 	tcsetattr(uart0_file, TCSANOW, &options); 
+
+}
+
+
+unsigned char ObterDados(int uart0_file)
+{			
+	if (uart0_file != -1)
+	{		
+	 		unsigned char rx_buffer[256];
+			unsigned char *ptr_buffer = rx_buffer;
+			int rx_length = -1;
+			char serial_data;
+
+			while (1)
+			{
+				rx_length = read(uart0_file, (void *) &serial_data, 1);
+				
+				if (rx_length < 0)
+				{
+					sleep(1);
+				}
+				else
+				{
+					if (serial_data == '\n')
+					{
+						*ptr_buffer++ = '\0';
+						break;
+					}
+					*ptr_buffer++ = serial_data;
+				}
+			}
 			
+			printf("%s\n", rx_buffer);
+			return rx_buffer;
+	} 
+}
+
+void QuebrarLinha(unsigned char protocolo)
+{
+	unsigned char *token;
+	unsigned char *coringa = ",";
+
+	token = strtok(protocolo, coringa);
+	
+	token = strtok(NULL, coringa); 
+}
+
+
+
+/*
+void ObterDados(int uart0_file)
+{			
 	if (uart0_file != -1)
 	{
 		while (1)
@@ -57,5 +157,4 @@ int main()
 		}
 	}
 
-	return (0);
-}
+}*/
