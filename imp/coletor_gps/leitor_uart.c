b9c88791ccc8bc28da1fb404db82caf8f017824b
Rpmoura7 <rpmoura7@gmail.com>
2017-08-04 13:09:34 +0000

    construção do dado gps

--- imp/coletor_gps/leitor_uart.c
+++ imp/coletor_gps/leitor_uart.c
@@ -5,11 +5,6 @@
 #include <unistd.h>
 #include <string.h>
 
-int inicializar_leitura(void);
-void definir_configuracoes(int);
-unsigned char *obter_dados(int);
-void construir_gps_data(unsigned char *);
-
 struct horario
 {
 	short int hora;
@@ -28,30 +23,44 @@ typedef struct data data_t;
 
 struct gps_data
 {
+	char *status;
 	horario_t horario;
 	float latitude;
+	char *latitude_o;
 	float longitude;
+	char *longitude_o;
 	float velocidade;
-	float angulo;
 	data_t data;	
 };
 typedef struct gps_data gps_data_t;
 
+int inicializar_leitura(void);
+void definir_configuracoes(int);
+char *obter_dados(int);
+char *extrair_gprmc_dados(char *);
+gps_data_t construir_gps_data(char *);
+
 int
 main(int argc, char **args)
 {
-	unsigned char *dados_gps;
-	// gps_data_t gps_data;
+	gps_data_t gps_data;
+	char *dados_gps;
+	char *d_gprmc;
 	int leitor_uart0;
-
+	
+	d_gprmc = NULL;
 	leitor_uart0 = inicializar_leitura();
 	definir_configuracoes(leitor_uart0);
 
 	while (1)
 	{
 	 	dados_gps = obter_dados(leitor_uart0);
-		construir_gps_data(dados_gps);
+		d_gprmc = extrair_gprmc_dados(dados_gps);
+
+		if (d_gprmc)
+			gps_data = construir_gps_data(d_gprmc);
 	}
+	free(d_gprmc);
 		 
 	return (0);
 }
@@ -72,7 +81,6 @@ inicializar_leitura(void)
 void
 definir_configuracoes(int uart0_file)
 {
-	
 	struct termios options;
 
 	tcgetattr(uart0_file, &options);
@@ -85,17 +93,17 @@ definir_configuracoes(int uart0_file)
 	tcsetattr(uart0_file, TCSANOW, &options); 
 }
 
-unsigned char *
+char *
 obter_dados(int uart0_file)
 {	
-	unsigned char *ptr_buffer;
+	char *ptr_buffer;
 	int rx_length;
 	char serial_data;
 	int i;
 
 	if (uart0_file != -1)
 	{
-		ptr_buffer = (unsigned char *) malloc(256 * sizeof(unsigned char));
+		ptr_buffer = (char *) malloc(256 * sizeof(char));
 	
 		if (!ptr_buffer)
 			exit(1);
@@ -116,33 +124,72 @@ obter_dados(int uart0_file)
 				ptr_buffer[i] = serial_data;
 			}
 		}
-		
-		printf("%s\n\n", ptr_buffer);
 	} 
 
 	return (ptr_buffer);
 }
 
-void
-construir_gps_data(unsigned char *mensagem_gps)
+char *
+extrair_gprmc_dados(char *mensagem_gps)
 {
-	unsigned char *token;	
-	unsigned char *token2;
-	unsigned char *delimitador = (unsigned char *) "MC";
+	const char *delimitador;
+	const char *id_gprmc;
+	char *split_mensagem_gps;
+	char *dado_gprmc;
 
-	int comparacao=0;
-	char *comparador = "$GPR"; 
+	delimitador = (const char *) "$";
+	id_gprmc = "GPRMC";
+	dado_gprmc = NULL;
 
-	token = strtok(mensagem_gps, delimitador);
-	token2 = strtok(NULL, delimitador);
-	 
-	comparacao = strcmp((char *)token, comparador);   //erro de segmentação.
-
-	if (comparacao == 0 && token2 != NULL)
-	{
-		printf("token1 = (%s)\n", token);	
-		printf("token2 = (%s)\n", token2);
-	}
-
+	for (split_mensagem_gps = strtok(mensagem_gps, delimitador); split_mensagem_gps != NULL; split_mensagem_gps = strtok(NULL, delimitador))
+		if (strncmp(split_mensagem_gps, id_gprmc, strlen(id_gprmc)) == 0)
+		{	
+			dado_gprmc = (char *) malloc(strlen(split_mensagem_gps) * sizeof(char));
+			strncat(dado_gprmc, split_mensagem_gps, strlen(split_mensagem_gps));
+			break;
+		}
 	free(mensagem_gps);
+	
+	return (dado_gprmc);
+}
+
+gps_data_t
+construir_gps_data(char *dado_gprmc)
+{
+	gps_data_t gps_data;
+	const char *delimitador;
+	char *split_dado_gprmc;
+	int index;
+	
+	delimitador = (const char *) ",";
+
+	for (split_dado_gprmc = strtok(dado_gprmc, delimitador), index = 0; split_dado_gprmc != NULL; split_dado_gprmc = strtok(NULL, delimitador), ++index)
+		switch (index)
+		{
+			case 1:
+				
+				gps_data.horario.hora = (int) split_dado_gprmc;
+				break;
+			case 2:
+				gps_data.status = (char *) split_dado_gprmc;
+				break;
+			case 3:
+				gps_data.latitude = (float) strtof(split_dado_gprmc, NULL);
+				break;
+			case 4:
+				gps_data.latitude_o = (char *) split_dado_gprmc;
+				break;
+	 		case 5:
+				break;
+			case 6:
+				break;
+			case 7:
+				break;
+			case 8:
+				break;
+
+		}
+	printf("%s %d %f %s\n", gps_data.status, gps_data.horario.hora, gps_data.latitude, gps_data.latitude_o);
+
+	return (gps_data);
 }
