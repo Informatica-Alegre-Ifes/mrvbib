b199fe4bf17dd4f812f8f8711a375a608372887d
Rpmoura7 <rpmoura7@gmail.com>
2017-09-07 22:23:47 +0000

    Construção do coletor Java

--- imp/coletor_gps/Serial.java
+++ imp/coletor_gps/Serial.java
@@ -1,20 +1,49 @@
+package coletor_gps;
+
 import java.io.FileReader;
 import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.FileNotFoundException;
 
-class ColetorGPS
+class Serial
 {
-	public static void main(String[] args)
+	private String mensagemNMEA;
+	private BufferedReader leitor;
+	
+	public Serial(String arquivo, String mensagemNMEA)
+	{
+		this.mensagemNMEA = mensagemNMEA;
+
+		inicializar(arquivo);
+	}
+	
+	public Dado obterDadoGPS()
 	{
 		try
 		{
-			BufferedReader leitor = new BufferedReader(new FileReader("/dev/ttyAMA0"));
 			String linha;
-			while ((linha = leitor.readLine()) != null)
-				if (linha.startsWith("$GPRMC"))
-					System.out.println(linha);
+
+			do
+			{
+				linha = leitor.readLine();
+			} while (linha == null || linha == "" || !linha.startsWith(mensagemNMEA));
+
+			return (new Dado(linha));
 		}
 		catch (IOException excecao)
+		{
+			System.out.println(excecao);
+			return (null);
+		}
+	}
+	
+	private void inicializar(String arquivo)
+	{
+		try
+		{
+			leitor = new BufferedReader(new FileReader(arquivo));
+		}
+		catch (FileNotFoundException excecao)
 		{
 			System.out.println(excecao);
 		}
