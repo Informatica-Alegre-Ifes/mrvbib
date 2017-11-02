d12fd648804b7b01172cf92d05d415ad42b0bd47
Rpmoura7 <rpmoura7@gmail.com>
2017-11-02 15:58:02 -0200

    Inclusao da biblioteca Pi4J no projeto

--- imp/coletor_gps/Coletor.java
+++ imp/coletor_gps/Coletor.java
@@ -1,16 +1,37 @@
 package coletor_gps;
 
+import java.io.IOException;
+
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.GpioPinDigitalOutput;
+import com.pi4j.io.gpio.RaspiPin;
+
 class Coletor
 {
 	public static void main(String[] args)
 	{
 		Serial serial = new Serial("/dev/ttyAMA0", "$GPRMC");
-		
+	
+		final GpioController gpio = GpioFactory.getInstance();
+		final GpioPinDigitalOutput ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
+	
+		//try
+		//{
+		//	Runtime.getRuntime().exec("gpio write 2 1");
+		//}
+		//catch (IOException excecao)
+		//{
+		//}
+
 		while (true)
 		{
 			Dado dado = serial.obterDadoGPS(28000);
 			if (dado != null && dado.ehValido())
+			{
 				dado.salvar();
+				ledPin.blink(1000, 15000);
+			}
 		}
 	}
 }
