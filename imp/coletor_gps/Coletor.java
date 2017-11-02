7fc566ec2778a6fa11fe49f3222619b80090d8d2
Rpmoura7 <rpmoura7@gmail.com>
2017-11-02 16:04:21 -0200

    Inclusao da biblioteca Pi4J no projeto

--- imp/coletor_gps/Coletor.java
+++ imp/coletor_gps/Coletor.java
@@ -16,21 +16,13 @@ class Coletor
 		final GpioController gpio = GpioFactory.getInstance();
 		final GpioPinDigitalOutput ledPin = gpio.provisionDigitalOutputPin(RaspiPin.GPIO_02);
 	
-		//try
-		//{
-		//	Runtime.getRuntime().exec("gpio write 2 1");
-		//}
-		//catch (IOException excecao)
-		//{
-		//}
-
 		while (true)
 		{
 			Dado dado = serial.obterDadoGPS(28000);
 			if (dado != null && dado.ehValido())
 			{
 				dado.salvar();
-				ledPin.blink(1000, 15000);
+				ledPin.blink(1000, 4000);
 			}
 		}
 	}
