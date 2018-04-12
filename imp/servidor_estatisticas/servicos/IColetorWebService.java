28faf3b58357a7a4e31be2dfd874e6edc5e8e88b
cayofontana <cayofontana@gmail.com>
2018-04-12 11:07:48 -0300

    refactoring

--- imp/servidor_estatisticas/servicos/IColetorWebService.java
+++ imp/servidor_estatisticas/servicos/IColetorWebService.java
@@ -4,6 +4,7 @@ import javax.jws.WebService;
 import javax.jws.WebMethod;
 import javax.jws.soap.SOAPBinding;
 import javax.jws.soap.SOAPBinding.Style;
+import java.util.List;
 
 @WebService
 @SOAPBinding(style = Style.RPC)
