40bbeadabcea73f319501c996f22f841afd21be7
cayofontana <cayofontana@gmail.com>
2018-06-25 02:14:39 -0300

    temporarios

--- test2.py
+++ test2.py
@@ -1,47 +1,41 @@
 import serial
 import RPi.GPIO as GPIO      
 import os, time
- 
+
 GPIO.setmode(GPIO.BOARD)    
- 
+
 # Enable Serial Communication
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
- 
+
 # Transmitting AT Commands to the Modem
 # '\r\n' indicates the Enter key
- 
-port.write('AT'+'\r')
+
+port.write('AT'+'\r\n')
 rcv = port.read(10)
 print rcv
 time.sleep(1)
- 
-port.write('ATE0'+'\r')      # Disable the Echo
+
+port.write('ATE0'+'\r\n')      # Disable the Echo
 rcv = port.read(10)
 print rcv
 time.sleep(1)
- 
-port.write('AT+CMGF=1'+'\r')  # Select Message format as Text mode 
+
+port.write('AT+CMGF=1'+'\r\n')  # Select Message format as Text mode 
 rcv = port.read(10)
 print rcv
 time.sleep(1)
- 
-port.write('AT+CNMI=2,1,0,0,0'+'\r')   # New SMS Message Indications
+
+port.write('AT+CNMI=2,1,0,0,0'+'\r\n')   # New SMS Message Indications
 rcv = port.read(10)
 print rcv
 time.sleep(1)
- 
+
 # Sending a message to a particular Number
- 
-port.write('AT+CMGW="+5527999150088"'+'\r')
+
+port.write('AT+CMGS="+5527999150088"'+'\r\n')
 rcv = port.read(10)
 print rcv
 time.sleep(1)
- 
-port.write('Teste - MRVBIB'+'\r')  # Message
+
+port.write('MRVBIB Test'+'\r\n')  # Message
 rcv = port.read(10)
-print rcv
- 
-port.write("\x1A") # Enable to send SMS
-for i in range(10):
-    rcv = port.read(10)
-    print rcv
