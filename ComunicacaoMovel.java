65a77ceb66065c335baa74ec0cc3d90282fe6258
cayofontana <cayofontana@gmail.com>
2018-06-26 09:33:14 -0300

    test temp

--- ComunicacaoMovel.java
+++ ComunicacaoMovel.java
@@ -1,6 +1,8 @@
 import java.io.IOException;
 import java.io.OutputStream;
-import java.io.InputStream;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import java.util.Enumeration;
 
