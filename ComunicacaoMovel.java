6386b31bacc9bd37bc85b751fa90549933999f34
cayofontana <cayofontana@gmail.com>
2018-07-16 11:43:08 -0300

    test sim900

--- ComunicacaoMovel.java
+++ ComunicacaoMovel.java
@@ -57,7 +57,7 @@ class ComunicacaoMovel
 		mensagensSIM.add("AT+CMGF=1" + "\r\n");
 		mensagensSIM.add("AT+CMGS=\"" + numeroCelular + "\"" + "\r\n");
 		mensagensSIM.add(mensagem + "\r");
-		mensagensSIM.add("\\\\x1a");
+		mensagensSIM.add(String.format("%X", 26));
 
 		// String mensagem1 = "AT";
 		// String mensagem2 = "ATE0";
