d6765e778c052f868f4577140f7992fee36b3f7e
cayofontana <cayofontana@gmail.com>
2018-07-22 15:43:15 -0300

    Ajustes nos testes do texto da mensagem SMS

--- imp/coletor_gps_new/Dado.java
+++ imp/coletor_gps_new/Dado.java
@@ -4,6 +4,8 @@ import java.util.Date;
 import java.util.List;
 import java.util.ArrayList;
 
+import java.text.DecimalFormat;
+
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
 import javax.xml.bind.annotation.XmlAttribute;
