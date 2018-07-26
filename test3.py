e22ff0f08e38ade2f94eca123f539a25436d08aa
cayofontana <cayofontana@gmail.com>
2018-07-26 10:35:02 -0300

    Teste SIM900 Http

--- test3.py
+++ test3.py
@@ -7,66 +7,86 @@ GPIO.setmode(GPIO.BOARD)
 # Enable Serial Communication
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
-port.write('AT'+'\r\n')
+port.write('AT+CIPMUX=1'+'\r\n')
 rcv = port.read(10)
 print rcv
 time.sleep(1)
 
-port.write('AT+CGATT?'+'\r\n')
+port.write('AT+CSTT="zap.vivo.com.br","vivo","vivo"'+'\r\n')
 rcv = port.read(10)
 print rcv
 time.sleep(1)
 
-port.write('AT+SAPBR=3,1,"CONTYPE","GPRS"'+'\r\n')
+port.write('AT+CIICR'+'\r\n')
 rcv = port.read(10)
 print rcv
 time.sleep(1)
 
-port.write('AT+SAPBR=3,1,"APN","zap.vivo.com.br"'+'\r\n')
+port.write('AT+CIFSR'+'\r\n')
 rcv = port.read(10)
 print rcv
 time.sleep(1)
 
-# port.write('AT+SAPBR=3,1,"USER","vivo"'+'\r\n')
+# port.write('AT'+'\r\n')
 # rcv = port.read(10)
 # print rcv
 # time.sleep(1)
 
-# port.write('AT+SAPBR=3,1,"PWD","vivo"'+'\r\n')
+# port.write('AT+CGATT?'+'\r\n')
 # rcv = port.read(10)
 # print rcv
 # time.sleep(1)
 
-port.write('AT+SAPBR=1,1'+'\r\n')
-rcv = port.read(10)
-print rcv
-time.sleep(1)
+# port.write('AT+SAPBR=3,1,"CONTYPE","GPRS"'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
 
-port.write('AT+HTTPINIT'+'\r\n')
-rcv = port.read(10)
-print rcv
-time.sleep(1)
+# port.write('AT+SAPBR=3,1,"APN","zap.vivo.com.br"'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
 
-port.write('AT+HTTPPARA="CID",1'+'\r\n')
-rcv = port.read(10)
-print rcv
-time.sleep(1)
+# # port.write('AT+SAPBR=3,1,"USER","vivo"'+'\r\n')
+# # rcv = port.read(10)
+# # print rcv
+# # time.sleep(1)
 
-port.write('AT+HTTPPARA="URL","www.uproc.com.br/page.php?dat=788868856775757467484648464874"'+'\r\n')
-rcv = port.read(10)
-print rcv
-time.sleep(1)
+# # port.write('AT+SAPBR=3,1,"PWD","vivo"'+'\r\n')
+# # rcv = port.read(10)
+# # print rcv
+# # time.sleep(1)
 
-port.write('AT+HTTPACTION=0'+'\r\n')
-rcv = port.read(10)
-print rcv
-time.sleep(1)
+# port.write('AT+SAPBR=1,1'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
 
-port.write('AT+HTTPTERM'+'\r\n')
-rcv = port.read(10)
-print rcv
-time.sleep(1)
+# port.write('AT+HTTPINIT'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
 
-port.write('AT+SAPBR=0,1'+'\r')
-rcv = port.read(10)
-print rcv
+# port.write('AT+HTTPPARA="CID",1'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
+
+# port.write('AT+HTTPPARA="URL","www.uproc.com.br/page.php?dat=788868856775757467484648464874"'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
+
+# port.write('AT+HTTPACTION=0'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
+
+# port.write('AT+HTTPTERM'+'\r\n')
+# rcv = port.read(10)
+# print rcv
+# time.sleep(1)
+
+# port.write('AT+SAPBR=0,1'+'\r')
+# rcv = port.read(10)
+# print rcv
