7131df79b31039adabed1026dea92d4144a1b61f
cayofontana <cayofontana@gmail.com>
2018-07-26 10:17:06 -0300

    Teste SIM900 Http

---
+++ test3.py
@@ -0,0 +1,72 @@
+import serial
+import RPi.GPIO as GPIO      
+import os, time
+
+GPIO.setmode(GPIO.BOARD)    
+
+# Enable Serial Communication
+port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
+
+port.write('AT'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+CGATT?'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+SAPBR=3,1,"CONTYPE","GPRS"'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+SAPBR=3,1,"APN","zap.vivo.com.br"'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+SAPBR=3,1,"USER","vivo"'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+SAPBR=3,1,"PWD","vivo"'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+SAPBR=1,1'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+HTTPINIT'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+HTTPPARA="CID",1'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+HTTPPARA="URL","www.uproc.com.br/page.php?dat=788868856775757467484648464874"'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+HTTPACTION=0'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+HTTPTERM'+'\r\n')
+rcv = port.read(10)
+print rcv
+time.sleep(1)
+
+port.write('AT+SAPBR=0,1'+'\r')
+rcv = port.read(10)
+print rcv
