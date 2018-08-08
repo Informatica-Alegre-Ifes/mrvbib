debd0395d9d5596273dfa8a442cff5f0be532edc
cayofontana <cayofontana@gmail.com>
2018-08-08 09:39:26 -0300

    http test new

--- test4.py
+++ test4.py
@@ -8,31 +8,36 @@ GPIO.setmode(GPIO.BOARD)
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
 port.write('AT+CSQ'+'\r\n')
-rcv = port.read(20)
+rcv = port.read(200)
 print rcv
 time.sleep(10)
 
 port.write('AT+CGATT=1'+'\r\n')
-rcv = port.read(20)
+rcv = port.read(200)
 print rcv
 time.sleep(10)
 
-port.write('AT+CIPSTART="TCP","date.jsontest.com",80'+'\r\n')
-rcv = port.read(20)
+port.write('AT+CIPSTART="TCP","http://date.jsontest.com",80'+'\r\n')
+rcv = port.read(200)
 print rcv
 time.sleep(10)
 
 port.write('AT+CIPSEND'+'\r\n')
-rcv = port.read(20)
+rcv = port.read(200)
 print rcv
 time.sleep(10)
 
 port.write('GET /index.html HTTP/1.1'+'\r\n')
-rcv = port.read(20)
+rcv = port.read(200)
 print rcv
 time.sleep(10)
 
-port.write('HOST: date.jsontest.com'+'\r\n\n')
-rcv = port.read(20)
+port.write('HOST: http://date.jsontest.com'+'\r\n\n')
+rcv = port.read(200)
 print rcv
 time.sleep(10)
+
+port.write("\x1A")
+for i in range(10):
+	rcv = port.read(10)
+	print rcv
