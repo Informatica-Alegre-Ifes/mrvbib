4b35458e06dd194bf264564b3b2f4934114ae578
cayofontana <cayofontana@gmail.com>
2018-08-08 09:36:41 -0300

    http test new

--- test4.py
+++ test4.py
@@ -7,51 +7,32 @@ GPIO.setmode(GPIO.BOARD)
 # Enable Serial Communication
 port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 
-port.write('AT+SAPBR=3,1,"Contype","GPRS"'+'\r\n')
+port.write('AT+CSQ'+'\r\n')
 rcv = port.read(20)
 print rcv
 time.sleep(10)
 
-port.write('AT+SAPBR=3,1,"APN","zap.vivo.com.br"'+'\r\n')
+port.write('AT+CGATT=1'+'\r\n')
 rcv = port.read(20)
 print rcv
 time.sleep(10)
 
-port.write('AT+SAPBR=1,1'+'\r\n')
+port.write('AT+CIPSTART="TCP","date.jsontest.com",80'+'\r\n')
 rcv = port.read(20)
 print rcv
 time.sleep(10)
 
-port.write('AT+SAPBR=2,1'+'\r\n')
+port.write('AT+CIPSEND'+'\r\n')
 rcv = port.read(20)
 print rcv
 time.sleep(10)
 
-port.write('AT+HTTPINIT'+'\r\n')
+port.write('GET /index.html HTTP/1.1'+'\r\n')
 rcv = port.read(20)
 print rcv
 time.sleep(10)
 
-port.write('AT+HTTPPARA="CID",1'+'\r\n')
+port.write('HOST: date.jsontest.com'+'\r\n\n')
 rcv = port.read(20)
 print rcv
 time.sleep(10)
-
-port.write('AT+HTTPPARA="URL","http://date.jsontest.com\"'+'\r\n')
-rcv = port.read(20)
-print rcv
-time.sleep(10)
-
-port.write('AT+HTTPACTION=0'+'\r\n')
-rcv = port.read(20)
-print rcv
-time.sleep(10)
-
-port.write('AT+HTTPTERM'+'\r\n')
-rcv = port.read(20)
-print rcv
-time.sleep(10)
-
-port.write('AT+SAPBR=0,1'+'\r')
-rcv = port.read(20)
-print rcv
