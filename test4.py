2842fb78fe068e853ef3ec383e0f338313aabb93
cayofontana <cayofontana@gmail.com>
2018-08-09 19:56:45 -0300

    Teste HTTP SIM900

--- test4.py
+++ test4.py
@@ -20,12 +20,12 @@ port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 port.write('AT+CGATT=1'+'\r\n')
 rcv = port.read(200)
 print rcv
-time.sleep(2)
+time.sleep(1)
 
 port.write('AT+CGDCONT=1,"IP","zap.vivo.com.br"'+'\r\n')
 rcv = port.read(200)
 print rcv
-time.sleep(2)
+time.sleep(1)
 
 # port.write('AT+CIPSTATUS'+'\r\n')
 # rcv = port.read(200)
@@ -33,9 +33,9 @@ time.sleep(2)
 # time.sleep(2)
 
 port.write('AT+CIPSTART="TCP","172.217.30.83",80'+'\r\n')
-rcv = port.read(200)
+rcv = port.read(100)
 print rcv
-time.sleep(3)
+time.sleep(5)
 
 # port.write('AT+CIPSTATUS'+'\r\n')
 # rcv = port.read(200)
@@ -50,12 +50,17 @@ time.sleep(2)
 port.write('GET /index.html HTTP/1.1'+'\r\n')
 rcv = port.read(200)
 print rcv
-time.sleep(2)
+time.sleep(1)
 
 port.write('HOST: date.jsontest.com'+'\r\n')
 rcv = port.read(200)
 print rcv
 time.sleep(2)
+
+port.write("\x1A")
+rcv = port.read(300)
+print rcv
+time.sleep(3)
 
 # port.write('AT+CIPSTATUS'+'\r\n')
 # rcv = port.read(200)
@@ -67,7 +72,12 @@ rcv = port.read(200)
 print rcv
 time.sleep(2)
 
-port.write("\x1A")
-for i in range(5):
-	rcv = port.read(10)
-	print rcv
+port.write('AT+CIPSHUT'+'\r\n')
+rcv = port.read(200)
+print rcv
+time.sleep(1)
+
+# port.write("\x1A")
+# for i in range(5):
+# 	rcv = port.read(10)
+# 	print rcv
