72ddedc01469b5e961bd3d4e9b293e85886a40b6
cayofontana <cayofontana@gmail.com>
2018-08-12 20:37:40 -0300

    Ajuste no método enviarMensagemHTTP ->  instruções AT

--- imp/coletor_gps/ComunicacaoMovel.java
+++ imp/coletor_gps/ComunicacaoMovel.java
@@ -64,8 +64,8 @@ class ComunicacaoMovel implements IStatusProdutor
 		instrucoesAT.add(new InstrucaoAT("AT+CGDCONT=1,\"IP\",\"" + apn + "\"" + "\r\n", 1000));
 		instrucoesAT.add(new InstrucaoAT("AT+CIPSTART=\"TCP\",\"" + endereco + "\"," + porta + "\r\n", 5000));
 		instrucoesAT.add(new InstrucaoAT("AT+CIPSEND" + "\r", 2000));
-		instrucoesAT.add(new InstrucaoAT("GET " + documento + "?" + parametros + " HTTP/1.1" + "\r\n", 1000));
-		instrucoesAT.add(new InstrucaoAT("HOST: " + endereco + "\r\n", 2000));
+		instrucoesAT.add(new InstrucaoAT("GET /" + documento + "?" + parametros + " HTTP/1.1" + "\r\n", 1000));
+		instrucoesAT.add(new InstrucaoAT("HOST: " + endereco + "\r\n\r\n", 2000));
 		instrucoesAT.add(new InstrucaoAT("\u001a", 3000));
 		instrucoesAT.add(new InstrucaoAT("AT+CIPCLOSE" + "\r\n", 2000));
 		instrucoesAT.add(new InstrucaoAT("AT+CIPSHUT" + "\r\n", 1000));
