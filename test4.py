eb6e16de7ece57e0d8c9c2b4d8d65e01c7f9610f
cayofontana <cayocruz@gmail.com>
2018-10-12 22:05:43 +0000

    Ajustes no envio de mensagens HTTP

--- test4.py
+++ test4.py
@@ -18,12 +18,12 @@ port = serial.Serial("/dev/ttyS0", baudrate=9600, timeout=1)
 # time.sleep(2)
 
 port.write('AT+CGATT=1'+'\r\n')
-rcv = port.read(200)
+rcv = port.read(100)
 print rcv
 time.sleep(1)
 
 port.write('AT+CGDCONT=1,"IP","zap.vivo.com.br"'+'\r\n')
-rcv = port.read(200)
+rcv = port.read(100)
 print rcv
 time.sleep(5)
 
@@ -32,7 +32,7 @@ time.sleep(5)
 # print rcv
 # time.sleep(2)
 
-port.write('AT+CIPSTART="TCP","201.140.234.76",80'+'\r\n')
+port.write('AT+CIPSTART="TCP","201.140.234.76",8080'+'\r\n')
 rcv = port.read(100)
 print rcv
 time.sleep(5)
@@ -43,12 +43,12 @@ time.sleep(5)
 # time.sleep(2)
 
 port.write('AT+CIPSEND'+'\r')
-rcv = port.read(200)
+rcv = port.read(100)
 print rcv
 time.sleep(5)
 
-port.write('GET /teste.html HTTP/1.0'+'\r\n')
-rcv = port.read(200)
+port.write('GET /cadastroumidade.php?umidade=16 HTTP/1.0'+'\r\n')
+rcv = port.read(100)
 print rcv
 time.sleep(1)
 
@@ -62,20 +62,20 @@ rcv = port.read(12228)
 print rcv
 time.sleep(20)
 
-# port.write('AT+CIPSTATUS'+'\r\n')
-# rcv = port.read(200)
-# print rcv
-# time.sleep(2)
-
-port.write('AT+CIPCLOSE'+'\r\n')
-rcv = port.read(12228)
+port.write('AT+CIPSTATUS'+'\r\n')
+rcv = port.read(200)
 print rcv
 time.sleep(2)
 
-port.write('AT+CIPSHUT'+'\r\n')
-rcv = port.read(12228)
-print rcv
-time.sleep(1)
+# port.write('AT+CIPCLOSE'+'\r\n')
+# rcv = port.read(12228)
+# print rcv
+# time.sleep(2)
+
+# port.write('AT+CIPSHUT'+'\r\n')
+# rcv = port.read(12228)
+# print rcv
+# time.sleep(1)
 
 # port.write("\x1A")
 # for i in range(5):
