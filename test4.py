225b083b3963b13a7ee0f1e585cd5fddb575784c
cayo <informatica.al.ifes@gmail.com>
2018-10-12 19:33:15 -0300

    Alteração no tempo de espera

--- test4.py
+++ test4.py
@@ -55,12 +55,12 @@ time.sleep(1)
 port.write('HOST: 201.140.234.76'+'\r\n\r\n')
 rcv = port.read(12228)
 print rcv
-time.sleep(20)
+time.sleep(5)
 
 port.write("\x1A")
 rcv = port.read(12228)
 print rcv
-time.sleep(20)
+time.sleep(5)
 
 port.write('AT+CIPSTATUS'+'\r\n')
 rcv = port.read(200)
