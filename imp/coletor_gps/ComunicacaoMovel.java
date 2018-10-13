f2001cd306e9c1741667c94af711bbd56240522a
cayo <informatica.al.ifes@gmail.com>
2018-10-13 03:46:53 -0300

    Ajustes na lista que contém as instruções AT no método enviarMensagemHTTP

--- imp/coletor_gps/ComunicacaoMovel.java
+++ imp/coletor_gps/ComunicacaoMovel.java
@@ -61,14 +61,12 @@ class ComunicacaoMovel implements IStatusProdutor
 		List<InstrucaoAT> instrucoesAT = new ArrayList<InstrucaoAT>();
 
 		instrucoesAT.add(new InstrucaoAT("AT+CGATT=1" + "\r\n", 1000));
-		instrucoesAT.add(new InstrucaoAT("AT+CGDCONT=1,\"IP\",\"" + apn + "\"" + "\r\n", 1000));
+		instrucoesAT.add(new InstrucaoAT("AT+CGDCONT=1,\"IP\",\"" + apn + "\"" + "\r\n", 5000));
 		instrucoesAT.add(new InstrucaoAT("AT+CIPSTART=\"TCP\",\"" + endereco + "\"," + porta + "\r\n", 5000));
 		instrucoesAT.add(new InstrucaoAT("AT+CIPSEND" + "\r", 2000));
-		instrucoesAT.add(new InstrucaoAT("GET /" + documento + "?" + parametros + " HTTP/1.1" + "\r\n", 1000));
+		instrucoesAT.add(new InstrucaoAT("GET /" + documento + "?" + parametros + " HTTP/1.0" + "\r\n", 1000));
 		instrucoesAT.add(new InstrucaoAT("HOST: " + endereco + "\r\n\r\n", 2000));
 		instrucoesAT.add(new InstrucaoAT("\u001a", 3000));
-		instrucoesAT.add(new InstrucaoAT("AT+CIPCLOSE" + "\r\n", 2000));
-		instrucoesAT.add(new InstrucaoAT("AT+CIPSHUT" + "\r\n", 1000));
 
 		try
 		{
