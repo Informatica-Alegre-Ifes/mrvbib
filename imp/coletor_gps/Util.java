1f32ad13ec46c43d2d79ca97b191236c81c6e28f
cayo <informatica.al.ifes@gmail.com>
2018-10-13 22:33:53 -0300

    Inclusão do método executarInstrucaoConsole(String instrucao) e atualização no método obterEnderecoMAC().

--- imp/coletor_gps/Util.java
+++ imp/coletor_gps/Util.java
@@ -3,6 +3,8 @@ package coletor_gps;
 import java.io.FileNotFoundException;
 import java.io.InputStream;
 import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 import java.net.NetworkInterface;
 import java.net.SocketException;
